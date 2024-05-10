c3398013bb113b65ab29f359cd280d516ea34001
Lucas Camelo <lucascamelo62@gmail.com>
2024-05-09 21:23:00 -0300

    Comandos Describe e It

---
+++ cypress/e2e/examples/arrow.cy.js
@@ -0,0 +1,5 @@
+it('Nada agora', function () { })
+
+const soma = () => 5 + 5
+
+console.log(soma)
---
+++ cypress/e2e/examples/describe.cy.js
@@ -0,0 +1,15 @@
+/// <reference types="cypress" />
+
+describe ('Cadastro de Usuário', () => {
+    it('Fluxo Básico', () => {
+
+    })
+
+    it('Fluxo Alternativo', () => {
+
+    })
+
+    it('Fluxo Exceção', () => {
+        
+    })
+})
