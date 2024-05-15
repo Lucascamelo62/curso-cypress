7bd2c6235d9d06322c19c12a661a40c14d62906a
Lucas Camelo <lucascamelo62@gmail.com>
2024-05-14 22:45:07 -0300

    Buscando por elementos

--- cypress/e2e/basic.cy.js
+++ cypress/e2e/basic.cy.js
@@ -4,7 +4,7 @@ beforeEach(() => {
     cy.visit('https://wcaquino.me/cypress/componentes.html');
 })
 
-describe('Acessando uma página', () => {
+describe.skip('Acessando uma página', () => {
     it('Acessando URL aquino e encontrando title', () => {
         cy.title()
             .should('be.equal', 'Campo de Treinamento')
@@ -12,7 +12,15 @@ describe('Acessando uma página', () => {
     })
 
     it('Interagindo com o botão', () => {
-        cy.get('#buttonSimple').click();
-        cy.get('#buttonSimple').should('have.value', 'Obrigado!');
+        cy.get('#buttonSimple')
+            .click()
+            .should('have.value', 'Obrigado!');
+    })
+})
+
+describe('Interagindo com elementos da tela', () => {
+    it('Buscando elemento', () => {
+        cy.get('body').should('contain', 'Cuidado');
+        cy.get(':nth-child(4) > :nth-child(1) > label').should('have.text', 'Qual\n\t\t\t\t\t\t\t\tsua comida favorita?');
     })
 })
