5c6fb877b21bac74007e14d0502570a6f827ea79
camelo <lucascamelo62@gmail.com>
2024-05-20 21:09:16 -0300

    Interagindo com elemento input

--- cypress/e2e/basic.cy.js
+++ cypress/e2e/basic.cy.js
@@ -4,7 +4,7 @@ beforeEach(() => {
     cy.visit('https://wcaquino.me/cypress/componentes.html');
 })
 
-describe.skip('Acessando uma página', () => {
+describe('Acessando uma página', () => {
     it('Acessando URL aquino e encontrando title', () => {
         cy.title()
             .should('be.equal', 'Campo de Treinamento')
@@ -22,5 +22,23 @@ describe('Interagindo com elementos da tela', () => {
     it('Buscando elemento', () => {
         cy.get('body').should('contain', 'Cuidado');
         cy.get(':nth-child(4) > :nth-child(1) > label').should('have.text', 'Qual\n\t\t\t\t\t\t\t\tsua comida favorita?');
+    })
+})
+
+describe('Interagindo com inputs de texto', () => {
+    it('Encontrando elemento nome', () => {
+        cy.get('#formNome')
+        .type('Teste')
+        .should('have.value', 'Teste');
+    })
+
+    it('Encontrando elemento sugestão', () => {
+        cy.get('#elementosForm\\:sugestoes')
+        .type('Lucas Silva Camelo{backspace}')
+        .should('have.value', 'Lucas Silva Camel');
+    })
+
+    it('Encontrando elemento input', () => {
+        cy.get('#tabelaUsuarios > :nth-child(2) > :nth-child(1) > :nth-child(6) > input').type('OIOIOI{selectall}TCHAU');
     })
 })
